f614d127de7ca33c9c2792b06eea80b9a96d4796
Fabio Ivona <fabio.ivona@defstudio.it>
2020-09-10 13:58:07 +0200

    aggiunti helpers per le traduzioni
    Took 3 minutes

--- src/helpers.php
+++ src/helpers.php
@@ -10,3 +10,21 @@
             return app(Html::class);
         }
     }
+
+    if(!function_exists('Trans')){
+        /**
+         * @return \Illuminate\Contracts\Translation\Translator|string|array|null
+         */
+    	function Trans($key = null, array $replace = [], $locale = null){
+    	    return ucfirst(trans($key, $replace, $locale));
+        }
+    }
+
+    if(!function_exists('TRANS')){
+        /**
+         * @return \Illuminate\Contracts\Translation\Translator|string|array|null
+         */
+        function TRANS($key = null, array $replace = [], $locale = null){
+            return strtoupper(trans($key, $replace, $locale));
+        }
+    }

f6a5a7e70b91515af0b51a5c7faad762a22f6f90
Fabio Ivona <fabio.ivona@defstudio.it>
2020-09-10 14:52:37 +0200

    fix
    Took 46 seconds

--- src/helpers.php
+++ src/helpers.php
@@ -11,20 +11,20 @@
         }
     }
 
-    if(!function_exists('Trans')){
+    if(!function_exists('uc_trans')){
         /**
          * @return \Illuminate\Contracts\Translation\Translator|string|array|null
          */
-    	function Trans($key = null, array $replace = [], $locale = null){
-    	    return ucfirst(trans($key, $replace, $locale));
+        function uc_trans($key = null, array $replace = [], $locale = null){
+            return ucfirst(trans($key, $replace, $locale));
         }
     }
 
-    if(!function_exists('TRANS')){
+    if(!function_exists('u_trans')){
         /**
          * @return \Illuminate\Contracts\Translation\Translator|string|array|null
          */
-        function TRANS($key = null, array $replace = [], $locale = null){
+        function u_trans($key = null, array $replace = [], $locale = null){
             return strtoupper(trans($key, $replace, $locale));
         }
     }

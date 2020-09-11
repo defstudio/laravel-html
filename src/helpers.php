b4fe3a18bc73e0faf74cf62dfd5594c69ddd7a82
Fabio Ivona <fabio.ivona@defstudio.it>
2020-09-11 10:39:46 +0200

    fix
    Took 27 seconds

--- src/helpers.php
+++ src/helpers.php
@@ -16,6 +16,15 @@
          * @return \Illuminate\Contracts\Translation\Translator|string|array|null
          */
         function uc_trans($key = null, array $replace = [], $locale = null){
+            return ucwords(trans($key, $replace, $locale));
+        }
+    }
+
+    if(!function_exists('uf_trans')){
+        /**
+         * @return \Illuminate\Contracts\Translation\Translator|string|array|null
+         */
+        function uf_trans($key = null, array $replace = [], $locale = null){
             return ucfirst(trans($key, $replace, $locale));
         }
     }
